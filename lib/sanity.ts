0d3b439504e374a6692f85a8c58ce0f7c39d254c
RajatKapooor <rajat.kapoor@bba.christuniversity.in>
2026-07-05 14:08:04 +0530

    Updates from Ship Studio

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -8,6 +8,7 @@ export const client = createClient({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? '2024-01-01',
   useCdn: true,
+  perspective: 'published',
   token: process.env.SANITY_API_READ_TOKEN,
 })
 
